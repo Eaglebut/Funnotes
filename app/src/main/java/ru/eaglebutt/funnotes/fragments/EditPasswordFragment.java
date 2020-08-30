f480a1a59cae0993eeacd645cfe10f5053fc8076
Eaglebutt <Eaglebuttt@gmail.com>
2020-08-30 13:17:55 +0300

    finished edit password fragment

--- app/src/main/java/ru/eaglebutt/funnotes/fragments/EditPasswordFragment.java
+++ app/src/main/java/ru/eaglebutt/funnotes/fragments/EditPasswordFragment.java
@@ -4,62 +4,88 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import ru.eaglebutt.funnotes.R;
+import com.google.android.material.snackbar.Snackbar;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link EditPasswordFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import ru.eaglebutt.funnotes.R;
+import ru.eaglebutt.funnotes.databinding.FragmentEditPasswordBinding;
+import ru.eaglebutt.funnotes.model.User;
+import ru.eaglebutt.funnotes.repositories.UserRepository;
+
+
 public class EditPasswordFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private FragmentEditPasswordBinding binding;
+    private UserRepository repository;
+    private EditText oldPasswordEditText;
+    private EditText newPasswordEditText;
+    private EditText repeatNewPasswordEditText;
+    private Button saveButton;
+    private Button cancelButton;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public EditPasswordFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EditPasswordFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static EditPasswordFragment newInstance(String param1, String param2) {
-        EditPasswordFragment fragment = new EditPasswordFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+
+    public static EditPasswordFragment newInstance() {
+        return new EditPasswordFragment();
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_password, container, false);
+        binding = FragmentEditPasswordBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setUp();
+
+    }
+
+    private void setUp() {
+        repository = UserRepository.getInstance(getContext());
+        oldPasswordEditText = binding.oldPasswordEditTextEditPassword;
+        newPasswordEditText = binding.newPasswordEditTextEditPassword;
+        repeatNewPasswordEditText = binding.repeatNewPasswordEditTextEditPassword;
+        saveButton = binding.saveButtonEditPassword;
+        cancelButton = binding.cancelButtonEditPassword;
+
+        saveButton.setOnClickListener(v -> {
+            String oldPassword = oldPasswordEditText.getText().toString();
+            String newPassword = newPasswordEditText.getText().toString();
+            String repeatNewPassword = repeatNewPasswordEditText.getText().toString();
+            User user = repository.getObservableUser().get();
+
+            if (newPassword.isEmpty() || oldPassword.isEmpty() || repeatNewPassword.isEmpty()) {
+                Snackbar.make(getView(), R.string.missing_value_string, Snackbar.LENGTH_LONG).show();
+            } else if (!oldPassword.equals(user.getPassword())) {
+                Snackbar.make(getView(), R.string.wrong_password_string, Snackbar.LENGTH_LONG).show();
+            } else if (!newPassword.equals(repeatNewPassword)) {
+                Snackbar.make(getView(), R.string.passwords_not_match_string, Snackbar.LENGTH_LONG).show();
+            } else {
+                user.setPassword(newPassword);
+                repository.updateUser(user);
+                getActivity().onBackPressed();
+            }
+        });
+
+        cancelButton.setOnClickListener(v -> getActivity().onBackPressed());
+
+
     }
 }
