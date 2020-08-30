bca6a3ac907fd6afd8df7642a26de29c2c205f70
Eaglebutt <Eaglebuttt@gmail.com>
2020-08-30 14:27:25 +0300

    added datepicker and timepicker into create and edit tasks fragments

--- app/src/main/java/ru/eaglebutt/funnotes/fragments/EditTaskFragment.java
+++ app/src/main/java/ru/eaglebutt/funnotes/fragments/EditTaskFragment.java
@@ -1,6 +1,9 @@
 package ru.eaglebutt.funnotes.fragments;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +13,9 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.Calendar;
+
+import ru.eaglebutt.funnotes.R;
 import ru.eaglebutt.funnotes.databinding.FragmentEditTaskBinding;
 import ru.eaglebutt.funnotes.model.Event;
 import ru.eaglebutt.funnotes.repositories.EventRepository;
@@ -23,9 +29,12 @@ public class EditTaskFragment extends Fragment {
     private int id;
     private EventRepository repository;
     private FragmentEditTaskBinding binding;
+    private long completionTime = 0;
 
     private Button saveButton;
     private Button cancelButton;
+    private Button dateButton;
+    private Calendar dateAndTime;
 
     public EditTaskFragment() {
     }
@@ -55,6 +64,18 @@ public class EditTaskFragment extends Fragment {
         return binding.getRoot();
     }
 
+    private TimePickerDialog.OnTimeSetListener timeSetListener = (view, hourOfDay, minute) -> {
+        dateAndTime.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        dateAndTime.set(Calendar.MINUTE, minute);
+        setTimeButton();
+    };
+    private DatePickerDialog.OnDateSetListener dateSetListener = (view, year, month, dayOfMonth) -> {
+        dateAndTime.set(Calendar.YEAR, year);
+        dateAndTime.set(Calendar.MONTH, month);
+        dateAndTime.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        setTime();
+    };
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -62,16 +83,61 @@ public class EditTaskFragment extends Fragment {
         binding.setRepository(repository);
         saveButton = binding.saveTaskButtonEditTask;
         cancelButton = binding.cancelButtonEditTask;
+        dateButton = binding.pickDateButtonEditTask;
+
+        completionTime = repository.getEventList().get(id).getStartTime();
+        dateAndTime = Calendar.getInstance();
+        dateAndTime.setTimeInMillis(completionTime * 1_000);
+        setTimeButton();
 
         saveButton.setOnClickListener(v -> {
             Event event = repository.getEventList().get(id);
             event.setTitle(binding.taskTitleEditTask.getText().toString());
             event.setDescription(binding.taskDescriptionEditTask.getText().toString());
+            event.setStartTime(completionTime);
             repository.updateEvent(event);
             getActivity().onBackPressed();
         });
 
         cancelButton.setOnClickListener(v -> getActivity().onBackPressed());
 
+        dateButton.setOnClickListener(v -> {
+            setDate();
+        });
     }
+
+    public void setDate() {
+        DatePickerDialog dialog = new DatePickerDialog(getContext(), dateSetListener,
+                dateAndTime.get(Calendar.YEAR),
+                dateAndTime.get(Calendar.MONTH),
+                dateAndTime.get(Calendar.DAY_OF_MONTH));
+        dialog.show();
+        dialog.getButton(DatePickerDialog.BUTTON_POSITIVE)
+                .setTextColor(getResources().getColor(R.color.colorPrimaryText, null));
+        dialog.getButton(DatePickerDialog.BUTTON_NEGATIVE)
+                .setTextColor(getResources().getColor(R.color.colorPrimaryText, null));
+
+    }
+
+    public void setTime() {
+        TimePickerDialog dialog = new TimePickerDialog(getContext(), timeSetListener,
+                dateAndTime.get(Calendar.HOUR_OF_DAY),
+                dateAndTime.get(Calendar.MINUTE), true);
+        dialog.show();
+        dialog.getButton(TimePickerDialog.BUTTON_POSITIVE)
+                .setTextColor(getResources().getColor(R.color.colorPrimaryText, null));
+        dialog.getButton(TimePickerDialog.BUTTON_NEGATIVE)
+                .setTextColor(getResources().getColor(R.color.colorPrimaryText, null));
+
+    }
+
+    private void setTimeButton() {
+        dateButton.setText(DateUtils.formatDateTime(getContext(),
+                dateAndTime.getTimeInMillis(),
+                DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_SHOW_YEAR
+                        | DateUtils.FORMAT_SHOW_TIME));
+        completionTime = dateAndTime.getTimeInMillis() / 1_000;
+    }
+
+
 }
--- app/src/main/java/ru/eaglebutt/funnotes/fragments/NewTaskFragment.java
+++ app/src/main/java/ru/eaglebutt/funnotes/fragments/NewTaskFragment.java
@@ -1,6 +1,9 @@
 package ru.eaglebutt.funnotes.fragments;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +15,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+
+import java.util.Calendar;
 
 import ru.eaglebutt.funnotes.R;
 import ru.eaglebutt.funnotes.model.Event;
@@ -29,6 +34,7 @@ public class NewTaskFragment extends Fragment {
     private Button cancelButton;
     private EventRepository eventRepository;
     private Toolbar toolbar;
+    private Calendar dateAndTime;
 
     public NewTaskFragment() {
     }
@@ -47,6 +53,18 @@ public class NewTaskFragment extends Fragment {
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_new_task, container, false);
     }
+
+    private TimePickerDialog.OnTimeSetListener timeSetListener = (view, hourOfDay, minute) -> {
+        dateAndTime.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        dateAndTime.set(Calendar.MINUTE, minute);
+        setTimeButton();
+    };
+    private DatePickerDialog.OnDateSetListener dateSetListener = (view, year, month, dayOfMonth) -> {
+        dateAndTime.set(Calendar.YEAR, year);
+        dateAndTime.set(Calendar.MONTH, month);
+        dateAndTime.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        setTime();
+    };
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
@@ -69,8 +87,45 @@ public class NewTaskFragment extends Fragment {
         cancelButton.setOnClickListener(v -> {
             getActivity().onBackPressed();
         });
+
+        dateButton.setOnClickListener(v -> {
+            dateAndTime = Calendar.getInstance();
+            setDate();
+        });
     }
 
+    public void setDate() {
+        DatePickerDialog dialog = new DatePickerDialog(getContext(), dateSetListener,
+                dateAndTime.get(Calendar.YEAR),
+                dateAndTime.get(Calendar.MONTH),
+                dateAndTime.get(Calendar.DAY_OF_MONTH));
+        dialog.show();
+        dialog.getButton(DatePickerDialog.BUTTON_POSITIVE)
+                .setTextColor(getResources().getColor(R.color.colorPrimaryText, null));
+        dialog.getButton(DatePickerDialog.BUTTON_NEGATIVE)
+                .setTextColor(getResources().getColor(R.color.colorPrimaryText, null));
+
+    }
+
+    public void setTime() {
+        TimePickerDialog dialog = new TimePickerDialog(getContext(), timeSetListener,
+                dateAndTime.get(Calendar.HOUR_OF_DAY),
+                dateAndTime.get(Calendar.MINUTE), true);
+        dialog.show();
+        dialog.getButton(TimePickerDialog.BUTTON_POSITIVE)
+                .setTextColor(getResources().getColor(R.color.colorPrimaryText, null));
+        dialog.getButton(TimePickerDialog.BUTTON_NEGATIVE)
+                .setTextColor(getResources().getColor(R.color.colorPrimaryText, null));
+
+    }
+
+    private void setTimeButton() {
+        dateButton.setText(DateUtils.formatDateTime(getContext(),
+                dateAndTime.getTimeInMillis(),
+                DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_SHOW_YEAR
+                        | DateUtils.FORMAT_SHOW_TIME));
+        completionTime = dateAndTime.getTimeInMillis() / 1000;
+    }
 
     private void findAllViews(View view) {
         toolbar = view.getRootView().findViewById(R.id.app_toolbar);
